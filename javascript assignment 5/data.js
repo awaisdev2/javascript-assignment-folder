5eb5f583655484fadffc111d9794390afbdadb15
Awais <awaisdev2@gmail.com>
2023-08-17 21:46:10 +0500

    updated folder

--- javascript assignment 5/data.js
+++
@@ -1,7 +0,0 @@
-const data = [
-    {name: 'Awais',email: 'awais@dev.com',contact: 9999},
-    {name: 'John',email: 'John@dev.com',contact: 7777},
-    {name: 'Doe',email: 'Doe@dev.com',contact: 8888},
-    {name: 'Alexa',email: 'Alexa@dev.com',contact: 5555}
-]
-module.exports = data
